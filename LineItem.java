d577e9509de34728cb4d1af235d60f8ed63c46d5
Zak <jzstaab@gmail.com>
2015-01-06 13:45:36 -0800

    nothing really just class creation

---
+++ LineItem.java
@@ -0,0 +1,9 @@
+
+
+public class LineItem{
+	
+	public LineItem(String name, int quantity, double pricePerUnit){
+
+	
+	}
+}
---
+++ ShoppingCart.java
@@ -0,0 +1,11 @@
+
+
+public class ShoppingCart{
+	
+	//Create a new shopping cart
+	public ShoppingCart(){
+		
+
+	}
+
+}
