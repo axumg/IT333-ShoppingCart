57220980afdd086a86aec014149884d4a37c2d10
Zak <jzstaab@gmail.com>
2015-01-08 10:44:46 -0800

    updated shopping cart
    initialized the array at 0 instead of 10, and used to length to decide
    what to do so there aren't unnecessary iterations through the basket
    array and there shouldn't be any null value comparisons anymore

--- src/ShoppingCart.java
+++ src/ShoppingCart.java
@@ -10,31 +10,34 @@ public class ShoppingCart{
 
 	//Create a new shopping cart and initialize array
 	public ShoppingCart(){
-		basket = new LineItem[10];
+		basket = new LineItem[0];
 
 	}
 
 	public void add(LineItem item){
-		int count = 0;
-		while (count < 10){
-			if (basket[count] == null){
-				System.out.println("adding a(n) "+ item.name + " it's cost is "+  item.getCost());
-				basket[count] = item;
-				break;
-			}
-			else count++;
-		}
-		if (count == 10){
+
+		if (basket.length > 9) {
 			System.out.println("My cart is too full! Why can't I hold all of these limes?!?");
 		}
+		else{
+			LineItem[] tempBasket = new LineItem[basket.length + 1];
+			int count = 0;
+			for (LineItem thing : basket) {
+				tempBasket[count] = thing;
+				count++;
+			}
+			System.out.println("adding a(n) " + item.name + " it's cost is " + item.getCost());
+			tempBasket[count] = item;
+			basket = tempBasket;
+		}
+
+
 	}
 
 	public double getTotalCost(){
 		double total = 0;
 		for(LineItem item : basket){
-			if (item != null) {
 				total += item.getCost();
-			}
 		}
 
 		return total;
